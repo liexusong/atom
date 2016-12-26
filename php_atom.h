e429bfc78d5f2c5838ca359add979bbf944d9f5d
liexusong <liexusong@qq.com>
2016-12-27 00:03:27 +0800

    update functions

--- php_atom.h
+++ php_atom.h
@@ -24,7 +24,7 @@
 extern zend_module_entry atom_module_entry;
 #define phpext_atom_ptr &atom_module_entry
 
-#define PHP_ATOM_VERSION "0.1.0" /* Replace with version number for your extension */
+#define PHP_ATOM_VERSION "0.5.0" /* Replace with version number for your extension */
 
 #ifdef PHP_WIN32
 #	define PHP_ATOM_API __declspec(dllexport)
@@ -38,9 +38,9 @@ extern zend_module_entry atom_module_entry;
 #include "TSRM.h"
 #endif
 
-/* 
+/*
   	Declare any global variables you may need between the BEGIN
-	and END macros here:     
+	and END macros here:
 
 ZEND_BEGIN_MODULE_GLOBALS(atom)
 	long  global_value;
@@ -48,12 +48,12 @@ ZEND_BEGIN_MODULE_GLOBALS(atom)
 ZEND_END_MODULE_GLOBALS(atom)
 */
 
-/* In every utility function you add that needs to use variables 
-   in php_atom_globals, call TSRMLS_FETCH(); after declaring other 
+/* In every utility function you add that needs to use variables
+   in php_atom_globals, call TSRMLS_FETCH(); after declaring other
    variables used by that function, or better yet, pass in TSRMLS_CC
    after the last function argument and declare your utility function
    with TSRMLS_DC after the last declared argument.  Always refer to
-   the globals in your function as ATOM_G(variable).  You are 
+   the globals in your function as ATOM_G(variable).  You are
    encouraged to rename these macros something shorter, see
    examples in any other php module directory.
 */
